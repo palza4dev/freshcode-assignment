6d9d859f2e6820885d1736ef7f0f519f4742729a
김지훈 <kimvz2088@gmail.com>
2021-11-04 21:36:54 +0900

    create "유저 view.py 작성" - decorator.py 작성_로그인/ 어드민 권한 부여 기능 추가 - 토큰으로 이용한 로그인

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,117 @@
-from django.test import TestCase
+import json
+from os import RTLD_NODELETE
+import bcrypt
 
-# Create your tests here.
+from django.test  import TestCase, Client
+
+from users.models import User
+
+
+class SigninTest(TestCase):
+    def setUp(self):
+        password = 'abc1234!'
+        User.objects.create(
+            id           = 1,
+            email        = 'user1@gmail.com',
+            password     = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            role         = 'admin'
+        )
+    
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signinview_post_success(self):
+        client = Client()
+        
+        user = {
+            'email'    : 'user1@gmail.com',
+            'password' : 'abc1234!',
+            'role'     : 'admin'
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+        access_token = response.json()['access_token']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 
+            {
+                'access_token' : access_token
+            }
+        )
+
+    def test_signinview_post_invalid_email(self):
+        client = Client()
+        
+        user = {
+            'email'    : 'user2@gmail.com',
+            'password' : 'abc1234!',
+            'role'     : 'admin'
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'INVALID_USER_EMAIL'
+            }
+        )
+    
+    def test_signinview_post_invalid_password(self):
+        client = Client()
+        
+        user = {
+            'email'    : 'user1@gmail.com',
+            'password' : 'abc1234@',
+            'role'     : 'admin'
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'INVALID_USER_PASSWORD'
+            }
+        )
+
+    def test_signinview_post_key_error_email(self):
+        client = Client()
+        
+        user = {
+            'password' : 'abc1234!',
+            'role'     : 'admin'
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'KEY_ERROR'
+            }
+        )
+
+    def test_signinview_post_key_error_password(self):
+        client = Client()
+        
+        user = {
+             'email' : 'user1@gmail.com',
+             'role'  : 'admin'
+        }
+        response     = client.post('/users/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'KEY_ERROR'
+            }
+        )
+
+    def test_signinview_post_jsondecod_eerror(self):
+        client = Client()
+
+        response     = client.post('/users/signin')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                'message' : 'JSON_DECODE_ERROR'
+            }
+        )
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
+import re
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.http                import JsonResponse
+from django.views               import View
+from json.decoder               import JSONDecodeError
+from django.conf                import settings
+
+from users.models           import User
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'USER_DOSE_NOT_EXIST'}, status = 404)
+
+            user = User.objects.get(email=data['email'])
+
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status = 401)
+            
+            token = jwt.encode({'id' : user.id}, settings.SECRET_KEY, algorithm = settings.ALGORITHM)
+
+            return JsonResponse({'token':token}, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+
+        except JSONDecodeError:
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)
